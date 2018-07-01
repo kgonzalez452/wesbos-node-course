6651203b06da968458e8cfbd47d5efe2a6284d03
kgonzalez452 <kgonzalez@techlaunch.io>
2018-07-01 10:24:42 -0400

    On the last video for module 4, but for some reason when i try to create a new store, it doesnt work for me, so when i go to the database, it tells me that the topology was destroyed. it tried looking up what that meant, but couldnt find anything. so i closed and force quit my database application to see if reconnecting would help out. it did, it doesnt say that anymore but i still cant make a new store.

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -25,9 +25,16 @@ exports.addStore = (req, res) => {
     res.render('editStore', {
         title: 'Add a Store'
     });
+            exports.createStore = async (req, res) => {
+                const store = await (new Store(req.body)).save();
+                req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
+                res.redirect(`/store/${store.slug}`);
+            };
 }
-exports.createStore = async (req, res) => {
-    const store = await (new Store(req.body)).save();
-    req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
-    res.redirect(`/store/${store.slug}`);
+
+exports.getStores = async (req, res) => {
+    // 1. Query the database for a list of all stores
+    const stores = await Store.find();
+    // console.log(stores);
+    res.render('stores', { title: 'Stores', stores });
 };
